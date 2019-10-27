0f4a94e379b0e7e93c38dfe54cd33beeeb98536c
Md. Zulfikar Ali <zulfikaralizehan@gmail.com>
2019-10-27 19:21:09 +0600

    User permission & check testcase Enable bluetooth With Button

--- app/src/main/java/com/zulfikar/buetoothcarcontrol/MainActivity.java
+++ app/src/main/java/com/zulfikar/buetoothcarcontrol/MainActivity.java
@@ -1,13 +1,49 @@
 package com.zulfikar.buetoothcarcontrol;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button bluetoothOnBtn,bluetoothOffBtn;
+    BluetoothAdapter bluetoothAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bluetoothAdapter=BluetoothAdapter.getDefaultAdapter();
+        bluetoothOnBtn=(Button) findViewById(R.id.b_on);
+        bluetoothOffBtn=(Button) findViewById(R.id.b_off);
+
+    }
+
+    public void onBluetooth(){
+
+        if(bluetoothAdapter==null){
+            Toast.makeText(getApplicationContext(),"Your phone does not support bluetooth",Toast.LENGTH_LONG).show();
+        }
+        else {
+            if(!bluetoothAdapter.isEnabled()){
+                Intent intent=new Intent(bluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(intent,1);
+            }
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode==1) {
+            if (resultCode==RESULT_OK) {
+                Toast.makeText(getApplicationContext(), "Bluetooth enabled", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
